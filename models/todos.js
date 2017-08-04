09109392faea6ef94e0a0c574b002352ed4d363d
sw-yx <shawnthe1@gmail.com>
2017-08-04 11:36:44 -0400

    submission for deadline

--- models/todos.js
+++ models/todos.js
@@ -9,9 +9,29 @@ module.exports = {
   // ==== COMPLETE THE FOLLOWING (SEE `model.js` TEST SPEC) =====
   listPeople: function () {
     // returns an array of all people for whom tasks exist
+    return Object.keys(tasks)
   },
   add: function (name, task) {
     // saves a task for a given person
+    if (!task.hasOwnProperty('complete')) task.complete = false
+    if (tasks[name]) {
+      // var x = tasks[name]
+      // x
+      tasks[name].push(task)
+    } else {
+      tasks[name] = [task]
+    }
+    // return []
+  },
+  list: function (name) {
+    return tasks[name]
+  },
+  complete: function (name, idx) {
+    tasks[name][idx].complete = true
+  },
+  remove: function (name, idx) {
+    tasks[name].splice(idx,1)
   }
   // etc.
+
 };
--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,42 @@
 
 var express = require('express');
 var router = express.Router();
+var todos = require('../models/todos')
 module.exports = router;
 
 // write your routes here. Feel free to split into multiple files if you like.
+
+router.get('/users/:name/tasks', (req, res) => {
+    let newlist = todos.list(req.params.name)
+    if (newlist)
+        {
+            if (req.query.status == 'complete') newlist = newlist.filter(x => x.complete)
+            if (req.query.status == 'active') newlist = newlist.filter(x => !x.complete)
+            res.json(newlist)
+        }
+    else
+        {res.json(404, 'not found')}
+})
+router.post('/users/:name/tasks', (req, res) => {
+    if (Object.keys(req.body).map(x => ['complete','content'].includes(x)).reduce((a, b) => a && b)) {
+        todos.add(req.params.name, req.body)
+        const newlist = todos.list(req.params.name)
+        if (newlist)
+            {res.json(201, newlist[newlist.length - 1])}
+        else
+            {res.json(404, 'not found')}
+    } else {
+        res.json(400, 'done')
+    }
+})
+router.put('/users/:name/tasks/:id', (req, res) => {
+    todos.complete(req.params.name, req.params.id)
+    res.json('done')
+})
+router.delete('/users/:name/tasks/:id', (req, res) => {
+    todos.remove(req.params.name, req.params.id)
+    res.json(204, 'done')
+})
+router.get('/users', (req, res) => {
+    res.json(todos.listPeople())
+})
